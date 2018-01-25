2011b8c9e5ea12f24152b072ff917752ecfe1b98
Chris James <qui666@gmail.com>
2018-01-25 14:02:26 +0000

    creating new manuscript from webapp

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Redirect, Route } from 'react-router-dom'
 
 class App extends Component {
   render () {
@@ -35,16 +35,53 @@ const Home = () => (
   </div>
 )
 
-const ViewManuscript = ({match}) => (
-  <div>
-    <h3>Manuscript {match.params.entityId}</h3>
-  </div>
-)
+class ViewManuscript extends React.Component {
 
-const CreateManuscript = ({match}) => (
-  <div>
-    <h3>I will create a manuscript...</h3>
-  </div>
-)
+  constructor(props, context) {
+    super()
+    this.id = props.entityId
+  }
+
+  getManuscript(id) {
+    fetch(id)
+      .then(res => res.json())
+      .then(manuscript => this.setState({manuscript}))
+  }
+
+  render () {
+    if(this.state && this.state.manuscript) {
+      return (<div>
+        <h1><em>Title</em> {this.state.manuscript.Title}</h1>
+        <h2>Manuscript {this.entityId}</h2>
+        <article>
+          <h3>Abstract</h3>
+          {this.state.manuscript.Abstract}
+          </article>
+      </div>)
+    } else {
+      this.getManuscript(this.entityId)
+      return (<div>
+      </div>)
+    }
+  }
+}
+
+class CreateManuscript extends React.Component {
+
+  createNewManuscript () {
+    fetch('/manuscripts', {method: 'POST'})
+      .then(res => res.headers.get('location'))
+      .then(location => this.setState({location}))
+  }
+
+  render () {
+    if (this.state && this.state.location) {
+      return <Redirect to={this.state.location}/>
+    } else {
+      this.createNewManuscript()
+      return <p>Creating new manuscript</p>
+    }
+  }
+}
 
 export default App
